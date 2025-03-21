65eee49dff1ff312dd24ff2789f779707b9f4fe1
Leonel-Falkenstein <lfalkenstein@mail.austral.edu.ar>
2025-03-21 10:51:42 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma=(a+b)
+    diferencia=(a-b)
+    producto=(a*b)
+    promedio=((a+b)/2)
+    co_entero=(a//b)
+    resto=(a%b)
+    division=(a/b)
+    print(suma)
+    print(diferencia)
+    print(producto)
+    print(promedio)
+    print(co_entero)
+    print(resto)
+    print(division)
